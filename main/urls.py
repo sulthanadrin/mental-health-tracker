cdf126508d9a6341cd3f520d3aa11ca78e186347
Sulthan Adrin <sulthanadrin0806@gmail.com>
2024-09-18 14:00:05 +0700

    menyelesaikan Tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from main.views import show_main
 from main.views import create_mood_entry
-from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
-
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register
+from main.views import login_user, logout_user
 
 app_name = 'main'
 
@@ -12,6 +12,8 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
-path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
-    
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!k18u=h%&^p4*uz98ok5ekh(2z%c+jpra2#^3q^6%q-s%7c2^n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sultan-adrin-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
